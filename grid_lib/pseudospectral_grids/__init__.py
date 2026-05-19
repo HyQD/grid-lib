4e31f20d4bb0561812299b59641ea29142f15bd1
haakoek <h.e.kristiansen@kjemi.uio.no>
2026-05-19 15:45:55 +0200

    Export Linear_map from pseudospectral_grids package

--- grid_lib/pseudospectral_grids/__init__.py
+++ grid_lib/pseudospectral_grids/__init__.py
@@ -1,5 +1,5 @@
 from .femdvr import FEMDVR
 from .sinc_dvr import SincDVR
-from .gauss_legendre_lobatto import GaussLegendreLobatto
+from .gauss_legendre_lobatto import GaussLegendreLobatto, Linear_map
 
-__all__ = ["FEMDVR", "SincDVR", "GaussLegendreLobatto"]
+__all__ = ["FEMDVR", "SincDVR", "GaussLegendreLobatto", "Linear_map"]
